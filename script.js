fab16a3d9fdf0f110bfa8537eb51a2c48a8b17bc
Moyo Olojede <Moyo@Moyos-MBP.home>
2021-06-16 18:06:25 -0400

    Changes were made to Project 2

--- script.js
+++ script.js
@@ -1,36 +1,8 @@
 /*Build a background color switcher.Provide five color options for the user to select from, 
 and whatever color they select becomes the background color of the page. */
 
-var backgroundGreen = document.getElementById("green");
-var backgroundRed = document.getElementById("red");
-var backgroundBlue = document.getElementById("blue");
-var backgroundBlack = document.getElementById("black");
-var backgroundYellow = document.getElementById("yellow");
-
-changeColor();
-
-  backgroundGreen.addEventListener("click",()=> {
-    changeColor("green")
-});
-
-  backgroundRed.addEventListener("click",()=> {
-    changeColor("red")
-});
-
- backgroundBlue.addEventListener("click",()=> {
-    changeColor("blue")
-});
-
-backgroundBlack.addEventListener("click",()=> {
-    changeColor("black")
-});
-
-backgroundYellow.addEventListener("click",()=> {
-    changeColor("yellow")
-});
-
 function changeColor(color) {
-  document.body.style.backgroundColor = color;
-};
-
-
+var x = document.getElementById("colorPicker");
+var bgColor = x.options[x.selectedIndex].value;
+document.body.style.backgroundColor = bgColor;
+}
